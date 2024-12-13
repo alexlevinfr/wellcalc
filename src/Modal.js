6bf3fcb70934b1fc31736957bb6f6a28ca261f55
Alex Levin <alex.levin@forgerock.com>
2024-12-13 18:16:17 +0000

    hide unneeded elements when different option is selected.

--- src/Modal.js
+++ src/Modal.js
@@ -4,10 +4,10 @@ import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 
 export function Modal() {
-    const [selectedActivity, setSelectedActivity] = useState('Steps');
+    /*const [selectedActivity, setSelectedActivity] = useState('Steps');
     const handleActivityChange = (event) => {
         setSelectedActivity(event.target.value);
-      };
+      };*/
 
       const [activities, setActivities] = useState({
         runkm:'0.0',
@@ -17,7 +17,8 @@ export function Modal() {
         cyclekm:'0.0',
         cyclemiles:'0.0',
         activitymins:'0',
-        steps:'0'
+        steps:'0',
+        type:'Steps'
       });
 
     const handleActivitiesChange = (event) => {
@@ -29,8 +30,9 @@ export function Modal() {
 
 
     const handleActivitiesSubmit = e => {
+        var activityjson=JSON.stringify(activities);
         e.preventDefault();
-          console.log("Submitted the activies form. No one is listening");
+          console.log("Submitted the activies form.",activityjson);
 
       }
       return (<Popup trigger = { <button> Click to open modal </button>}
@@ -39,17 +41,18 @@ export function Modal() {
                         <div className='modal'>
                             <div className='content'>
                                 Record a new activity.
-                                {<select value={selectedActivity} onChange={handleActivityChange}>
+                                {<label>type<select value={activities.type} onChange={handleActivitiesChange} name="type">
                                 <option value="Run">Run</option>
                                 <option value="Walkd">Walk distance</option>
                                 <option value="Steps">Walk steps</option>
                                 <option value="Cycle">Cycle distance</option>
                                 <option value="Activitymins">Activity minutes</option>
-                                </select>}
+                                </select></label>}
                                 {<form onSubmit={handleActivitiesSubmit}>
-                                
+                                {activities.type === "Steps" ? 
                                 <label>number of steps:<input type="number" name="steps" value={activities.steps} onChange={handleActivitiesChange} />
-                                </label>
+                                </label> : null
+                                }
                                 <input type="submit" />
                                 </form> }
                             </div>
